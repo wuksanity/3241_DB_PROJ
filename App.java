92873249eca853af1e5b65d27403da26e185a96a
Walker Riley <riley.855@osu.edu>
2023-02-19 18:07:49 -0500

    nothing signif

--- App.java
+++ App.java
@@ -1,7 +1,16 @@
+import java.util.*;
+
 public class App {
 
-public static void main(String[] args) throws Exception {
-    System.out.println("Hello world!");
-}
+    public static void main(String[] args) throws Exception {
+
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Programming running...press (q) at any time to quit.\n");
+
+
+        input.close();
+
+    }
 
 }
